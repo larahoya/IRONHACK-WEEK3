4359be7f991334e9adc214b0db49b12aa42b7b6f
Lara Hoya <larascully@hotmail.com>
2015-11-11 17:48:15 +0100

    caesar cypher

--- cypher.js
+++ cypher.js
@@ -1,44 +1,60 @@
-function caesarCypher(message, shift) {
+var caesarCypher = (function() {
 
-  if (shift === undefined) {
-    shift = -3;
+  var caesar_shift = 3;
+  my_public_obj = {};
+
+  function caesarBuilder (shift) {
+    return function (message) {
+
+      if (shift === undefined) {
+        shift = -3;
+      }
+
+      function lowerLetter(value, shift) {
+        value += shift;
+        if(value < 97) {
+          value += 26;
+        } else if(value > 122) {
+          value -= 26;
+        }
+        return String.fromCharCode(value);
+      }
+
+      function upperLetter(value, shift) {
+        value += shift;
+        if(value < 65) {
+          value += 26;
+        } else if(value > 90) {
+          value -= 26;
+        }
+        return String.fromCharCode(value);
+      }
+
+      var letters = message.split('')
+
+      var new_letters = letters.map(function(char) {
+        value = char.charCodeAt(0);
+        if (value >= 97 && value <= 122) {
+          value = lowerLetter(value, shift);
+        } else if (value >= 65 && value <= 90) {
+          value = upperLetter(value, shift);
+        } else {
+          value = String.fromCharCode(value);
+        }
+        return value
+      })
+
+      return new_letters.join('');
+    }
   }
 
-  function lowerLetter(value, shift) {
-    value += shift;
-    if(value < 97) {
-      value += 26;
-    } else if(value > 122) {
-      value -= 26;
-    }
-    return String.fromCharCode(value);
-  }
+  my_public_obj.cypher = caesarBuilder(-caesar_shift);
+  my_public_obj.uncypher = caesarBuilder(caesar_shift);
 
-  function upperLetter(value, shift) {
-    value += shift;
-    if(value < 65) {
-      value += 26;
-    } else if(value > 90) {
-      value -= 26;
-    }
-    return String.fromCharCode(value);
-  }
+  return my_public_obj;
 
-  var letters = message.split('')
+})();
 
-  var new_letters = letters.map(function(char) {
-    value = char.charCodeAt(0);
-    if (value >= 97 && value <= 122) {
-      value = lowerLetter(value, shift);
-    } else if (value >= 65 && value <= 90) {
-      value = upperLetter(value, shift);
-    } else {
-      value = String.fromCharCode(value);
-    }
-    return value
-  })
+console.log(caesarCypher.cypher("pb uhdo qdph lv grqdog gxfn"));
 
-  return new_letters.join('');
-}
 
-console.log(caesarCypher("pb uhdo qdph lv grqdog gxfn"));
